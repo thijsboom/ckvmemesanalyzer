a6bc115fa14b0fa69d41a60b1ad132bd8dbfb6a2
Thijs Boom <twboom@hotmail.nl>
2021-02-04 22:34:00 +0100

    Added demo data

---
+++ src/assets/data.js
@@ -0,0 +1,22 @@
+const demo = [
+    {
+        "name": "epic handshake",
+        "type": "2 dingen die goed samen gaan"
+    },
+    {
+        "name": "third world success kid",
+        "type": "blij zijn"
+    },
+    {
+        "name": "success kid",
+        "type": "blij zijn"
+    },
+    {
+        "name": "third world skeptical kid",
+        "type": "domme dingen"
+    },
+    {
+        "name": "is this a pigeon",
+        "type": "domme dingen"
+    }
+]
