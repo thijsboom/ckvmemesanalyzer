e88e64c24ea705fbed95ed15b6fbd2a3cd3c724e
Thijs Boom <twboom@hotmail.nl>
2021-02-08 15:27:01 +0100

    Cleaned things up

--- src/js/utility.js
+++ src/js/utility.js
@@ -4,7 +4,6 @@ function print(printable) {
     for (let i = 0; i < analyzer.array.length; i++) {
         if (printable.frequencies[i] === undefined) { continue; }
         const content = '<tr>' + '<td>' + printable.frequencies[i].type + '</td>' + '<td>' + printable.frequencies[i].frequency + '</td>' + '</tr>'
-
         document.querySelector('#print').innerHTML += content
     }
 }
