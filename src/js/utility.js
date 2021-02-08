38c0060d9c5e584fe0a8462f5d4eb1d26cd0fe75
Thijs Boom <twboom@hotmail.nl>
2021-02-08 14:07:59 +0100

    Made printing nice

--- src/js/utility.js
+++ src/js/utility.js
@@ -1,6 +1,17 @@
 fetchData(url);
-function run() {
-    const dataToAnalyze = createArray(data);
-    return analyze(dataToAnalyze).frequencies
+
+function print(printable) {
+    for (let i = 0; i < analyzer.array.length; i++) {
+        if (printable.frequencies[i] === undefined) { continue; }
+        const content = '<tr>' + '<td>' + printable.frequencies[i].type + '</td>' + '<td>' + printable.frequencies[i].frequency + '</td>' + '</tr>'
+
+        document.querySelector('#print').innerHTML += content
+    }
 }
 
+function run() {
+    const dataToAnalyze = createArray(data);
+    let out = analyze(dataToAnalyze)
+    print(out);
+    return out.frequencies.value;  
+}
